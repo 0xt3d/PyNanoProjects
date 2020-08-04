5d232c257969eca067cd5adb932c05bc879dad4a
Aditya <aditya.patil0404@gmail.com>
2020-08-04 06:22:51 +0530

    Added timer for 3600 seconds

--- PyNotifier/Notifier.py
+++ PyNotifier/Notifier.py
@@ -1,9 +1,12 @@
+import time as t
 from pynotifier import Notification
 
-Notification(
-    title = "Get UP & Drink water",
-    description = "It's been an hour have a quick Break",
-    icon_path = r'C:\Users\Aditya\Documents\[P]Projects\PyNotifier\Frame 1.ico',
-    duration = 30,
-    urgency = Notification.URGENCY_CRITICAL
-).send()
+while True:
+    t.sleep(3600)
+    Notification(
+        title = "Get UP & Drink water",
+        description = "It's been an hour have a quick Break",
+        icon_path = r'C:\Users\Aditya\Documents\[P]Projects\PyNotifier\Frame 1.ico',
+        duration = 30,
+        urgency = Notification.URGENCY_CRITICAL
+    ).send()
