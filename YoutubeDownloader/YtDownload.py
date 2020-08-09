0da228cc02fb03e51a83173b689b0c69b1226266
Aditya <aditya.patil0404@gmail.com>
2020-08-09 20:52:28 +0530

    Minor Changes

--- YoutubeDownloader/YtDownload.py
+++ YoutubeDownloader/YtDownload.py
@@ -1,6 +1,6 @@
 from pytube import YouTube, Playlist
 
 
-yt = YouTube('https://www.youtube.com/watch?v=ekYbYywaFYo')
+yt = YouTube('https://www.youtube.com/watch?v=Nxs_mpWt2BA')
 stream = yt.streams.first()
 stream.download('C://users//aditya//downloads')
